43c7ba1d5452320c82d7e2d92e4620cbfd3607c9
Vlad Dyranov <v.dyranov.d@gmail.com>
2025-05-04 00:06:33 +0300

    update processing.py (add func sort by date)

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,12 @@
-def filter_by_state(data: list[dict], state: str="EXECUTED") -> list:
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция, которая возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению."""
 
     return [i for i in data if i["state"] == state]
+
+
+def sort_by_date(data: list[dict], sort_order: str = "убывание") -> list[dict]:
+    """Функция, которая сортирует список по дате"""
+    if sort_order == "убывание":
+        return sorted(data, key=lambda x: x["date"], reverse=True)
+    return sorted(data, key=lambda x: x["date"])
