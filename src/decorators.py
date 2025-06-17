72ef4ba732fdd73e68bd9f8c20454d90609bb4f2
Vlad Dyranov <v.dyranov.d@gmail.com>
2025-06-17 13:50:04 +0300

    fixes

--- src/decorators.py
+++ src/decorators.py
@@ -3,6 +3,14 @@ from typing import Any, Callable, Optional
 
 
 def log(filename: Optional[str] = None) -> Callable:
+    """
+    Декоратор для логирования результатов выполнения функции.
+    При успешном вызове функции записывает сообщение <имя_функции> ok.
+    В случае исключения — логирует тип ошибки и переданные аргументы, затем повторно выбрасывает исключение.
+    Если указан путь к файлу filename, лог записывается в файл (в режиме append).
+    Иначе сообщение выводится в консоль
+    """
+
     def inner(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
