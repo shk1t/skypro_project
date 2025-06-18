fcba8f7f219a900068f4f584d30afb3551e154ed
Vlad Dyranov <v.dyranov.d@gmail.com>
2025-06-18 20:45:40 +0300

    add new functions in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,16 @@
 import json
-from src.logger_config import setup_logger
+import pandas as pd
+from logger_config import setup_logger
 from typing import Optional
 
 logger = setup_logger("utils.py", "logs/utils.log")
 
 
-def load_transactions(filename: str) -> list[Optional[dict]]:
+def load_transactions_json(filename: str) -> list[Optional[dict]]:
     """
     Загружает список транзакций из JSON-файла.
     """
+    logger.info("Работа с JSON-файлом")
     try:
         with open(filename, "r", encoding="utf-8") as file:
             data = json.load(file)
@@ -24,4 +26,28 @@ def load_transactions(filename: str) -> list[Optional[dict]]:
         logger.error(f"Ошибка декодирования JSON в файле: {filename}")
         return []
 
-load_transactions("data/operations.json")
+
+def load_transactions_csv(filename: str) -> list[Optional[dict]]:
+    """
+    Загружает список транзакций из CSV-файла.
+    """
+    logger.info("Работа с JSON-файлом")
+    try:
+        csv_data = pd.read_csv("data/transactions.csv", sep=";")
+        logger.info("Файл успешно прочитан")
+        return csv_data.to_dict("records")
+    except Exception as e:
+        logger.error(f"Ошибка: {e}")
+
+
+def load_transactions_excel(filename: str) -> list[Optional[dict]]:
+    """
+    Загружает список транзакций из EXCEL-файла.
+    """
+    logger.info("Работа с JSON-файлом")
+    try:
+        excel_data = pd.read_excel("data/transactions_excel.xlsx")
+        logger.info("Файл успешно прочитан")
+        return excel_data.to_dict("records")
+    except Exception as e:
+        logger.error(f"Ошибка: {e}")
