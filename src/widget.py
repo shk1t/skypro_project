6fe49b35d825960658441f727bbdc5b2028d6d8a
Vlad Dyranov <v.dyranov.d@gmail.com>
2025-09-08 14:51:26 +0300

    add main

--- src/widget.py
+++ src/widget.py
@@ -1,29 +1,50 @@
-import src.masks as ms
+import masks as ms
+from datetime import datetime as dt
+from logger_config import setup_logger
 
+logger = setup_logger("widget.py", "logs/widget.log")
 
 def mask_account_card(card_info: str) -> str:
-    """Функция, которая возвращает маску номера карты вместе с названием"""
+    """
+    Функция, которая возвращает маску номера карты или счета вместе с названием.
+    """
+    if not card_info:
+        logger.info(f"Счет не указан")
+        return "Нет данных"
+    if not isinstance(card_info, str):
+        logger.warning(f"Данные неккоректны: {card_info}")
+        return "Некорректные данные"
+    # Поиск первой цифры
+    try:
+        logger.info(f"Поиск первой цифры")
+        first_digit_index = next(i for i, c in enumerate(card_info) if c.isdigit())
+    except StopIteration:
+        logger.warning(f"Нет цифр в строке: {card_info}")
+        return "Некорректные данные"  # Нет цифр в строке
 
-    number_card = (
-        card_info[card_info.index(next(i for i in card_info if i.isdigit())):] if isinstance(card_info, str) else ""
-    )
+    number_card = card_info[first_digit_index:]
 
-    if number_card.isdigit():
-        mask_number_card = (
-            ms.get_mask_account(int(number_card))
-            if card_info[:4].lower() == "счет"
-            else ms.get_mask_card_number(int(number_card))
-        )
+    if not number_card.isdigit():
+        logger.warning("Неверный формат данных карты/счета")
+        return "Некорректные данные"
 
-        return card_info.replace(number_card, mask_number_card)
-    return "Некорректные данные"
+    try:
+        if card_info[:first_digit_index].strip().lower() == "счет":
+            masked_number = ms.get_mask_account(number_card)
+            logger.info(f"Маска счета: {masked_number}")
+        else:
+            masked_number = ms.get_mask_card_number(number_card)
+            logger.info(f"Маска номера карты: {masked_number}")
+    except Exception:
+        logger.warning("Некорректные данные")
+        return "Некорректные данные"  # например, слишком короткое число или ошибка в `ms`
+
+    return card_info.replace(number_card, masked_number)
 
 
 def get_date(date: str) -> str:
     """Функция, которая возвращает дату в формате ДД.ММ.ГГГГ"""
 
     return (
-        date[8:10] + "." + date[5:7] + "." + date[:4]
-        if isinstance(date, str) and len(date) == 26
-        else "Некорректные данные"
+        dt.fromisoformat(date)
     )
